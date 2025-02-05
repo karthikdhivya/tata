14791907a777abf366b4b84322ce3d7228f3e0dc
karthikdhivya <147987922+karthikdhivya@users.noreply.github.com>
2025-02-05 23:47:25 +0400

    Update tataapp.py

--- tataapp.py
+++ tataapp.py
@@ -6,7 +6,7 @@ st.title('TataPowerStock')
 
 ticker=st.sidebar.text_input("Enter Stock Ticker","TATAPOWER.NS")
 start_date=st.sidebar.text_input("Start Date","2020-01-01")
-end_date=st.sidebar.text_input("End date","2025-02-01")
+end_date=st.sidebar.text_input("End date","2023-01-01")
 
 @st.cache_data
 def load_data(ticker,start_date,end_date):
